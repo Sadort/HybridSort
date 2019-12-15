e2ad6cf6678932f9aedc16671bb51ce6c2ec304b
Sadort <hhsadort@gmail.com>
2019-12-15 15:40:16 -0800

    BLINE CPU

---
+++ tests/BLine/BLine_CPU.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <parallel/algorithm>
+#include <stdlib.h>
+#include <iostream>
+#include <omp.h>
+
+#define nthreads 16
+
+int main()
+{
+    uint64_t number_of_elements = 512L*1024*1024;
+    std::vector<uint64_t> h_key_array(number_of_elements);
+
+    for (uint64_t i = 0; i < number_of_elements; i++) {
+        h_key_array[i] = ((uint64_t)rand()) << 32 | (uint64_t)rand();
+    }
+
+    double start = omp_get_wtime();
+
+    #pragma omp parallel num_threads(nthreads)
+    	__gnu_parallel::sort(h_key_array.begin(), h_key_array.end());
+
+    double end = omp_get_wtime();
+    printf("%d threads, elapsed time is %f\n", nthreads, end-start);
+    return 0;
+}
--- tests/BLine/smake.sh
+++ tests/BLine/smake.sh
@@ -1,7 +1,10 @@
 rm -rf *.o
 
-#unified memory thrust::sort
+#base line thrust::sort
 /usr/local/cuda/bin/nvcc  -c -I/usr/local/cuda/include BLine_Thrust.cu
 /usr/local/cuda/bin/nvcc  -o BLine_Thrust.out BLine_Thrust.o 
+
+#cpu openmp gnu parallel
+g++ test_openmp.cpp -o test_openmp.out -fopenmp
 
 rm -rf *.o
