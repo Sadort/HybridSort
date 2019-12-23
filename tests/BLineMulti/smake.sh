84f3301a8477597698d69b6878906865e13015bb
Sadort <hhsadort@gmail.com>
2019-12-22 16:00:16 -0800

    multiway merge parallelism problem solved

--- tests/BLineMulti/smake.sh
+++ tests/BLineMulti/smake.sh
@@ -2,8 +2,7 @@ rm -rf *.o
 
 #base line thrust::sort
 /usr/local/cuda/bin/nvcc -c -I/usr/local/cuda/include Thrust_kernel.cu
-/usr/local/cuda/bin/nvcc -c -I/usr/local/cuda/include BLineMulti_Thrust.cpp
-/usr/local/cuda/bin/nvcc -o BLineMulti_Thrust.out -Xcompiler -fopenmp -O3 BLineMulti_Thrust.o Thrust_kernel.o -L/usr/local/cuda/lib64 -L/usr/lib/x86_64-linux-gnu -lnvidia-ml -lcudart
+/usr/local/cuda/bin/nvcc -o BLineMulti_Thrust.out -Xcompiler -fopenmp -Xcompiler -O3 BLineMulti_Thrust.cpp Thrust_kernel.o -L/usr/local/cuda/lib64 -L/usr/lib/x86_64-linux-gnu -lnvidia-ml -lcudart
 
 #cpu openmp gnu parallel
 #g++ BLine_CPU.cpp -o BLine_CPU.out -O3 -fopenmp
