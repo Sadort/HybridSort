a293863a55a043591bdce37a03f1bd1c9a92d305
Junjie Huang <junjiehuang@Junjies-MacBook-Pro.local>
2020-01-01 01:41:46 -0800

    Added cached allocator. Now sorting and memcpy are overlapping.

--- tests/PipeData/Thrust_kernel.cu
+++ tests/PipeData/Thrust_kernel.cu
@@ -1,14 +1,121 @@
 #include <stdio.h>
 #include <thrust/functional.h>
 #include <thrust/execution_policy.h>
+#include <thrust/system/cuda/vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+#include <thrust/generate.h>
+#include <thrust/pair.h>
+#include <map>
+#include <cassert>
 #include "PipeData_Thrust.h"
 
 #define start_index_s0 2*i*batch_size
 #define start_index_s1 2*i*batch_size+batch_size
 #define start_index_s2 2*i*batch_size+2*batch_size
 
+// cached_allocator: a simple allocator for caching allocation requests
+class cached_allocator
+{
+  public:
+    // just allocate bytes
+    typedef char value_type;
+
+    cached_allocator() {}
+
+    ~cached_allocator()
+    {
+      // free all allocations when cached_allocator goes out of scope
+      free_all();
+    }
+
+    char *allocate(std::ptrdiff_t num_bytes)
+    {
+      char *result = 0;
+
+      // search the cache for a free block
+      free_blocks_type::iterator free_block = free_blocks.find(num_bytes);
+
+      if(free_block != free_blocks.end())
+      {
+        std::cout << "cached_allocator::allocator(): found a hit" << std::endl;
+
+        // get the pointer
+        result = free_block->second;
+
+        // erase from the free_blocks map
+        free_blocks.erase(free_block);
+      }
+      else
+      {
+        // no allocation of the right size exists
+        // create a new one with cuda::malloc
+        // throw if cuda::malloc can't satisfy the request
+        try
+        {
+          std::cout << "cached_allocator::allocator(): no free block found; calling cuda::malloc" << std::endl;
+
+          // allocate memory and convert cuda::pointer to raw pointer
+          result = thrust::cuda::malloc<char>(num_bytes).get();
+        }
+        catch(std::runtime_error &e)
+        {
+          throw;
+        }
+      }
+
+      // insert the allocated pointer into the allocated_blocks map
+      allocated_blocks.insert(std::make_pair(result, num_bytes));
+
+      return result;
+    }
+
+    void deallocate(char *ptr, size_t n)
+    {
+      // erase the allocated block from the allocated blocks map
+      allocated_blocks_type::iterator iter = allocated_blocks.find(ptr);
+      std::ptrdiff_t num_bytes = iter->second;
+      allocated_blocks.erase(iter);
+
+      // insert the block into the free blocks map
+      free_blocks.insert(std::make_pair(num_bytes, ptr));
+    }
+
+  private:
+    typedef std::multimap<std::ptrdiff_t, char*> free_blocks_type;
+    typedef std::map<char *, std::ptrdiff_t>     allocated_blocks_type;
+
+    free_blocks_type      free_blocks;
+    allocated_blocks_type allocated_blocks;
+
+    void free_all()
+    {
+      std::cout << "cached_allocator::free_all(): cleaning up after ourselves..." << std::endl;
+
+      // deallocate all outstanding blocks in both lists
+      for(free_blocks_type::iterator i = free_blocks.begin();
+          i != free_blocks.end();
+          ++i)
+      {
+        // transform the pointer to cuda::pointer before calling cuda::free
+        thrust::cuda::free(thrust::cuda::pointer<char>(i->second));
+      }
+
+      for(allocated_blocks_type::iterator i = allocated_blocks.begin();
+          i != allocated_blocks.end();
+          ++i)
+      {
+        // transform the pointer to cuda::pointer before calling cuda::free
+        thrust::cuda::free(thrust::cuda::pointer<char>(i->first));
+      }
+    }
+
+};
+
+
 void PipeDataSort(uint64_t *h_key_array, uint64_t *d_key_array[2], uint64_t number_of_elements, uint64_t batch_size, uint64_t pinned_M_size, int nstreams = 2)
 {
+    cached_allocator alloc;
     int number_of_batches = number_of_elements / batch_size;
     int number_of_buffers = 2 * batch_size / pinned_M_size;
     
@@ -47,7 +154,7 @@ void PipeDataSort(uint64_t *h_key_array, uint64_t *d_key_array[2], uint64_t numb
                 }
             }
             if (s == 0) {
-                thrust::sort(thrust::cuda::par.on(streams[0]), th_key_array[0], th_key_array[0]+batch_size);
+                thrust::sort(thrust::cuda::par(alloc).on(streams[0]), th_key_array[0], th_key_array[0]+batch_size);
                 
                 for (int b = 0; b < number_of_buffers; b++) {
                     std::memcpy(pinned_M[1],
@@ -64,7 +171,7 @@ void PipeDataSort(uint64_t *h_key_array, uint64_t *d_key_array[2], uint64_t numb
                 }
             }
             else if (s == 1) {
-                thrust::sort(thrust::cuda::par.on(streams[1]), th_key_array[1], th_key_array[1]+batch_size);
+                thrust::sort(thrust::cuda::par(alloc).on(streams[1]), th_key_array[1], th_key_array[1]+batch_size);
                 
                 for (int b = 0; b < number_of_buffers; b++) {
                     cudaMemcpyAsync(pinned_M[0],
