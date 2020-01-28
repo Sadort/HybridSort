ec3c12dc8b3c01faee7451d326001ca372b15d0a
Junjie Huang <junjiehuang@Junjies-MacBook-Pro.local>
2020-01-28 03:09:18 -0800

    optimized Merge and ParMemcpy

--- tests/BLineMulti_Ext4_ParMemcpy/Merge_kernel.cpp
+++ tests/BLineMulti_Ext4_ParMemcpy/Merge_kernel.cpp
@@ -7,27 +7,6 @@
 #include <nvToolsExt.h>
 #include <sys/time.h>
 
-void PairMerge(uint64_t *key_array_1, uint64_t *key_array_2, uint64_t batch_size, int nthreads)
-{
-    omp_set_dynamic(false);
-    omp_set_num_threads(nthreads);
-    nvtxRangeId_t id1 = nvtxRangeStart("Pairwise-merge");
-    
-    std::vector< std::pair<uint64_t*, uint64_t*> > batches;
-    batches.push_back(std::make_pair(&key_array_1[0], &key_array_1[batch_size]));
-    batches.push_back(std::make_pair(&key_array_2[0], &key_array_2[batch_size]));
-    uint64_t *output_v = (uint64_t *)malloc(2*batch_size*sizeof(uint64_t));
-    
-    __gnu_parallel::multiway_merge(batches.begin(), batches.end(), output_v, 2*batch_size, std::less<uint64_t>());
-
-    std::memcpy(key_array_1, output_v, (batch_size)*sizeof(uint64_t));
-    std::memcpy(key_array_2, &output_v[batch_size], (batch_size)*sizeof(uint64_t));
-        
-    nvtxRangeEnd(id1);
-   
-    return;
-}
-
 void ParMemcpy(uint64_t *dest, uint64_t *src, int number_of_elements, int nthreads)
 {
     omp_set_dynamic(false);
@@ -41,3 +20,27 @@ void ParMemcpy(uint64_t *dest, uint64_t *src, int number_of_elements, int nthrea
     }
     return;
 }
+
+void PairMerge(uint64_t *key_array_1, uint64_t *key_array_2, uint64_t batch_size, int nthreads)
+{
+    omp_set_dynamic(false);
+    omp_set_num_threads(nthreads);
+    nvtxRangeId_t id1 = nvtxRangeStart("Pairwise-merge");
+    
+    std::vector< std::pair<uint64_t*, uint64_t*> > batches;
+    batches.push_back(std::make_pair(&key_array_1[0], &key_array_1[batch_size]));
+    batches.push_back(std::make_pair(&key_array_2[0], &key_array_2[batch_size]));
+    uint64_t *output_v = (uint64_t *)malloc(2*batch_size*sizeof(uint64_t));
+    
+    __gnu_parallel::multiway_merge(batches.begin(), batches.end(), output_v, 2*batch_size, std::less<uint64_t>());
+
+    //std::memcpy(key_array_1, output_v, (batch_size)*sizeof(uint64_t));
+    //std::memcpy(key_array_2, &output_v[batch_size], (batch_size)*sizeof(uint64_t));
+    ParMemcpy(key_array_1, output_v, batch_size, mem_threads);
+    ParMemcpy(key_array_2, &output_v[batch_size], batch_size, mem_threads);
+        
+    nvtxRangeEnd(id1);
+   
+    return;
+}
+
