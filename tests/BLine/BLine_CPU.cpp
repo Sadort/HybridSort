49cb80caa18582b6cda159fafa2dd61c39326525
Junjie Huang <junjiehuang@ucr-secure-43-10-13-222-21.wnet.ucr.edu>
2020-02-03 15:10:21 -0800

    added openmp threads to BLine_CPU

--- tests/BLine/BLine_CPU.cpp
+++ tests/BLine/BLine_CPU.cpp
@@ -22,23 +22,41 @@ int main()
     struct timeval CPUstart;
     gettimeofday(&CPUstart, NULL);
 
-    omp_set_dynamic(false);
-    omp_set_num_threads(nthreads);
+    int mem_threads = (int)log2((float)nthreads);
+    mem_threads = (int)exp2((float)mem_threads);
 
+    omp_set_dynamic(false);
+    omp_set_num_threads(mem_threads);
     uint64_t *indices = (uint64_t *)malloc(number_of_elements*sizeof(uint64_t));
-    for (uint64_t i = 0; i < number_of_elements; i++) {
-        indices[i] = i;
+    #pragma omp parallel
+    {
+        uint64_t tid = omp_get_thread_num();
+        uint64_t len = number_of_elements / nthreads;
+        uint64_t start_ind = tid * len;
+        for (uint64_t i = start_ind; i < start_ind + len; i++) {
+            indices[i] = i;
+        }
     }
     //printf("Real number of threads: %d\n", omp_get_num_threads());
+    omp_set_dynamic(false);
+    omp_set_num_threads(nthreads);
     __gnu_parallel::sort(indices, indices+number_of_elements, SortIndices(h_key_array));
 
     //std::sort(h_key_array.begin(), h_key_array.end(), __gnu_parallel::parallel_tag());
 
+    omp_set_dynamic(false);
+    omp_set_num_threads(mem_threads);
     uint64_t *sorted_key = (uint64_t *)malloc(number_of_elements*sizeof(uint64_t));
     uint64_t *sorted_value = (uint64_t *)malloc(number_of_elements*sizeof(uint64_t));
-    for (uint64_t i = 0; i < number_of_elements; i++) {
-        sorted_key[i] = h_key_array[indices[i]];
-        sorted_value[i] = h_value_array[indices[i]];
+    #pragma omp parallel
+    {
+        uint64_t tid = omp_get_thread_num();
+        uint64_t len = number_of_elements / nthreads;
+        uint64_t start_ind = tid * len;
+        for (uint64_t i = start_ind; i < start_ind + len; i++) {
+            sorted_key[i] = h_key_array[indices[i]];
+            sorted_value[i] = h_value_array[indices[i]];
+        }
     }
 
     struct timeval CPUend;
