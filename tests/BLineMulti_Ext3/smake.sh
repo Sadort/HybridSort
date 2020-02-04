8ee60fc233c66ba4064a2232b15445e52e63777f
Junjie Huang <junjiehuang@ucr-secure-43-10-13-220-59.wnet.ucr.edu>
2020-02-04 13:36:56 -0800

    BLineMulti_Ext3 commit

--- tests/BLineMulti_Ext3/smake.sh
+++ tests/BLineMulti_Ext3/smake.sh
@@ -1,9 +1,9 @@
 rm -rf *.o
 
 #base line thrust::sort
-/usr/local/cuda/bin/nvcc -Xcompiler -fopenmp -c -I/usr/local/cuda/include Merge_kernel.cpp -std=c++11
-/usr/local/cuda/bin/nvcc -c -I/usr/local/cuda/include Thrust_kernel.cu -std=c++11
-/usr/local/cuda/bin/nvcc -o BLineMulti_Thrust.out -Xcompiler -fopenmp -Xcompiler -O3 BLineMulti_Thrust.cpp Thrust_kernel.o Merge_kernel.o -L/usr/local/cuda/lib64 -L/usr/lib/x86_64-linux-gnu -lnvidia-ml -lcudart -lnvToolsExt -std=c++11
+/usr/local/cuda/bin/nvcc -Xcompiler -fopenmp -c -I/usr/local/cuda/include -I../../include Merge_kernel.cpp -std=c++11
+/usr/local/cuda/bin/nvcc -c -I/usr/local/cuda/include -I../../include Thrust_kernel.cu -std=c++11
+/usr/local/cuda/bin/nvcc -o BLineMulti_Thrust.out -Xcompiler -fopenmp -Xcompiler -O3 BLineMulti_Thrust.cpp Thrust_kernel.o Merge_kernel.o -I../../include -L/usr/local/cuda/lib64 -L/usr/lib/x86_64-linux-gnu -lnvidia-ml -lcudart -lnvToolsExt -std=c++11
 
 #cpu openmp gnu parallel
 #g++ BLine_CPU.cpp -o BLine_CPU.out -O3 -fopenmp
